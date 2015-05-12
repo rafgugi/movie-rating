2692e5948fdd18fc1914bf2d0b9d6a14725a0a52
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-05-12 19:34:07 +0700

    nyobak nyobak

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,15 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("it wotks!"));
+    }
+
+    public static Result greet(String name) {
+    	return ok(greet.render(name)).as("text/html");
+    }
+
+    public static Result home() {
+    	return ok(app.render("title", home.render()));
     }
 
 }
