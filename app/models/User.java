b2026001d2fd7c0bc2a6581a0289999cb13485e0
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-05-19 06:17:33 +0700

    user model fix

--- app/models/User.java
+++ app/models/User.java
@@ -13,6 +13,12 @@ public class User extends Model {
 
     @Id
     public Long id;
+
+    public int age;
+    public char gender;
+    public String occupation;
+    public String zip_code;
+    public String password;
     
     public static Finder<Long,User> find = new Finder<Long,User>(
         Long.class, User.class
